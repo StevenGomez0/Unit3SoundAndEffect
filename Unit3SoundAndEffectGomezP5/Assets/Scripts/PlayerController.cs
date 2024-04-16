62723bbe3b7a413792573bbb9752946ae6212440
Steven Gomez <719513@student.dusd.net>
2024-04-16 12:58:07 -0700

    3.4 done, unit 3 done
    added vfx for running and dying
    added sfx for jumping and dying
    added background music
    gonna do challenge 3 next

--- Unit3SoundAndEffectGomezP5/Assets/Scripts/PlayerController.cs
+++ Unit3SoundAndEffectGomezP5/Assets/Scripts/PlayerController.cs
@@ -7,15 +7,21 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public float jumpForce = 10;
     public float gravityModifier;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
     private bool grounded;
     public bool gameOver;
     private Animator playerAnim;
+    public AudioClip jumpSFX;
+    public AudioClip deathSFX;
+    private AudioSource playerAudio;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,6 +31,8 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             grounded = false;
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSFX, 1.0f);
             playerAnim.SetTrigger("Jump_trig");
         }
     }
@@ -35,11 +43,15 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             grounded = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             playerAnim.SetBool("Death_b", true);
             gameOver = true;
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(deathSFX, 1.0f);
             Debug.Log("Game Over");
         }
     }
