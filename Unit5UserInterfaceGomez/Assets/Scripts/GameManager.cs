80cdc0419600c86114a5a6acb22b22987208197b
Steven Gomez <719513@student.dusd.net>
2024-04-26 13:19:38 -0700

    5.3 and 5.4 done
    added game over screen and restart button
    made game over screen and restart button appear on game over (fruit drops to ground)
    made objects not spawn on game over
    made restart button reload scene
    
    added title screen with game title and difficulty buttons
    clicking difficulty button starts game now
    difficulty impacts how fast fruits spawn
    made title setup disappear on game start

--- Unit5UserInterfaceGomez/Assets/Scripts/GameManager.cs
+++ Unit5UserInterfaceGomez/Assets/Scripts/GameManager.cs
@@ -2,31 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor.EditorTools;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    public GameObject titleScreen;
+
+    public Button restartButton;
+    public bool isGameActive;
     private int score;
     private float spawnRate = 1;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     IEnumerator SpawnTarget()
     {
-        while(true)
+        while(isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int Index = Random.Range(0, targets.Count);
@@ -38,5 +33,29 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
     }
 }
